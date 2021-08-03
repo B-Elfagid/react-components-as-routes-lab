e2ddadd39d12aecbafef9eb049b151c3551e04ed
Betty <betty_elfagid@yahoo.co.uk>
2021-08-03 23:30:55 +0100

    commit tree 1504f2da179f245af4a9643496883c17e7f19dcc

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,8 +4,23 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
-    </div>
+       <h1>Movies Page</h1>
+         {movies.map((movie, index) => (
+           <div key={index}>
+           <h3>{movie.title}</h3>
+           <p>{movie.time} minutes</p>
+           <p>Genres:</p>
+            <ul>
+              {movie.genres.map((genre, index) => (
+                <li key={index}>
+                  {genre}
+                </li>
+              ))}
+            </ul>
+           </div>
+         )
+         )}
+       </div>
   );
 };
 
